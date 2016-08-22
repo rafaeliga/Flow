9248e3a95756ad3f975702c4df743f351d0032bd
Rafael Iga <rafael.f.iga@gmail.com>
2016-08-22 16:20:38 -0300

    Remove build from gitignore

---
+++ build/android-armeabi/flow/ui/css_node.c.d
@@ -0,0 +1,103 @@
+build/android-armeabi/flow/ui/css_node.c.o: flow/ui/css_node.c \
+  include/rubymotion.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/assert.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/stdlib.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/string.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/malloc.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/alloca.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/strings.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/stdint.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/asm/types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/memory.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/jni.h \
+  flow/ui/css_layout.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/math.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/internal_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/syslimits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/asm/page.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/stdio.h
+
+include/rubymotion.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/assert.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/stdlib.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/string.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/alloca.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/strings.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/memory.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/jni.h:
+
+flow/ui/css_layout.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/math.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/linux/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/machine/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/asm/page.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-arm/usr/include/stdio.h:
---
+++ build/android-x86/flow/ui/css_node.c.d
@@ -0,0 +1,109 @@
+build/android-x86/flow/ui/css_node.c.o: flow/ui/css_node.c \
+  include/rubymotion.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/assert.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/cdefs.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/cdefs_elf.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/android/api-level.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/stdlib.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/string.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/malloc.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/alloca.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/strings.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/stdint.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/posix_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/stddef.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/compiler.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/posix_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/posix_types_32.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/kernel.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/sysmacros.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/memory.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/jni.h \
+  flow/ui/css_layout.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/math.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/internal_types.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/limits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/syslimits.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/page.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/page_32.h \
+  /Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/stdio.h
+
+include/rubymotion.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/assert.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/android/api-level.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/stdlib.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/string.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/malloc.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/alloca.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/strings.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/stdint.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/posix_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/stddef.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/compiler.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/posix_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/posix_types_32.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/kernel.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/sysmacros.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/memory.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/jni.h:
+
+flow/ui/css_layout.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/math.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/linux/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/internal_types.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/machine/limits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/sys/syslimits.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/page.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/asm/page_32.h:
+
+/Users/rafaeliga/.rubymotion-android/ndk/platforms/android-16/arch-x86/usr/include/stdio.h:
